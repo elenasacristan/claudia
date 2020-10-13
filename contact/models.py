005eb0994b9b503764047d75092c8dcecf53d03d
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-10-13 17:57:10 +0100

    Improve resposiveness and add logic to contact view

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,42 @@
 from django.db import models
+from datetime import datetime
 
 class Contact(models.Model):
 
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, blank=True)
     message = models.TextField()
-
-    class Meta:
-        verbose_name_plural = "Contact_emails"
+    contact_date = models.DateField(default=datetime.now)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = "Emails"
+        ordering = ('contact_date',)
+
+    # email validation
+    def clean_name(self):
+                name = self.cleaned_data.get('name')
+                
+                if not name:
+                    raise forms.ValidationError(u'This field is required..')
+                return name
+
+    # email validation
+    def clean_email(self):
+                email = self.cleaned_data.get('email')
+                
+                if not email:
+                    raise forms.ValidationError(u'This field is required.')
+                return email
+
+    # content validation
+    def clean_message(self):
+                message = self.cleaned_data.get('message')
+                
+                if not message:
+                    raise forms.ValidationError(u'This field is required.')
+                return message
 
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.utils import timezone
 from .forms import ContactForm
 
 def contact(request):
-    contact_form = ContactForm()
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)    
+    
+        if contact_form.is_valid():
+            form = contact_form.save(commit=False)
+            form.contact_date = timezone.now()
+            form.save()
+            text = "Thanks {}, I have received your message!".format(form.name)
+            messages.success(request, text)
+            return redirect(reverse('index'))
+
+    else:
+        contact_form = ContactForm()
+
     return render(request, "contact.html", {"contact_form":contact_form})
